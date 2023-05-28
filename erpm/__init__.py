f888f42f9f82d9614f7a414efc709067ec0384ae
nawazmyasir <115599931+nawazmyasir@users.noreply.github.com>
2023-05-28 08:38:18 +0800

    Update __init__.py

--- erpm/__init__.py
+++ erpm/__init__.py
@@ -97,7 +97,7 @@ erpm_plugin = CredentialPlugin(
             #'type': 'string',
             #'help_text': 'The name of the Shared Credential List in ERPM System to fetch.'
         #}],
-        'required': ['url', 'username', 'password', 'account_name', 'shared_credential_list', 'system_name'],
+        'required': ['url', 'erpm_username', 'erpm_password', 'account_name', 'shared_credential_list', 'system_name'],
     },
     # backend is a callable function which will be passed all of the values
     # defined in `inputs`; this function is responsible for taking the arguments,
