51a9466bf17c7d25ebed321298bc1d87c607a385
nawazmyasir <115599931+nawazmyasir@users.noreply.github.com>
2023-05-30 17:31:22 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = []  # add Python dependencies here
 
 setup(
     name='erpm',
-    version='0.6',
+    version='0.7',
     author='Ansible, Inc.',
     author_email='info@ansible.com',
     description='',
